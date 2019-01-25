fc99db500bb58d77ca77e4fb1b8167fb88427cd7
Saikrishna <saikrishna.edupuganti@intel.com>
2019-01-25 15:11:06 -0800

    Enable sriov-network-device-plugin vfio mode
    Adding a cni to mirror MAC address of VF on a veth pair with results
    from ipam applied to enable DPDK apps to configure themselves when
    operating sriov-network-device-plugin in vfio mode
    
    Updated helper sriov.sh to allow for binding VFs to vfio-pci on boot
    
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>

--- clr-k8s-examples/9-multi-network/systemd/sriov.sh
+++ clr-k8s-examples/9-multi-network/systemd/sriov.sh
@@ -1,14 +1,50 @@
 #!/bin/bash
 # Usage: sriov.sh ens785f0 ens785f1 ...
 
-for pf in "$@"; do
+set -o errexit
+set -o pipefail
+set -o nounset
+
+reset_pf() {
+	local pf=$1
 	echo "Resetting $pf"
 	echo 0 | tee /sys/class/net/$pf/device/sriov_numvfs
+}
 
-	NUM_VFS=$(cat /sys/class/net/$pf/device/sriov_totalvfs)
+set_pf() {
+	local pf=$1
+	local NUM_VFS=$(cat /sys/class/net/$pf/device/sriov_totalvfs)
 	echo "Enabling $NUM_VFS for $pf"
 	echo $NUM_VFS | tee /sys/class/net/$pf/device/sriov_numvfs
 	ip link set $pf up
-	#for ((i = 0 ; i < ${NUM_VFS} ; i++ )); do ip link set $pf vf $i spoofchk off; done
-	for ((i = 0; i < ${NUM_VFS}; i++)); do ip link set dev $pf vf $i state enable; done
+}
+
+bind_vfs_vfio() {
+	local pf=$1
+	local pci=$(readlink /sys/devices/pci*/*/*/net/$pf/device | awk '{print substr($1,10)}')
+	echo "Binding VFs of PF $pf ($pci) to vfio-pci"
+	for i in $(ls -l /sys/devices/pci*/*/$pci | awk '"virtfn"==substr($9,1,6) {print substr($11,4)}'); do
+		echo $i | tee /sys/bus/pci*/*/$i/driver/unbind
+		echo vfio-pci | tee /sys/devices/pci*/*/$i/driver_override
+		echo $i | tee /sys/bus/pci/drivers/vfio-pci/bind
+	done
+}
+
+setup_vfs() {
+	local pf=$1
+	local NUM_VFS=$(cat /sys/class/net/$pf/device/sriov_totalvfs)
+	echo "Setting up VFs of PF $pf"
+	for ((i = 0; i < ${NUM_VFS}; i++)); do
+		ip link set dev $pf vf $i state enable
+		ip link set dev $pf vf $i mac \
+			$(printf '00:80:86:%02X:%02X:%02X\n' $((RANDOM % 256)) $((RANDOM % 256)) $((RANDOM % 256)))
+		# ip link set $pf vf $i spoofchk off
+	done
+}
+
+for pf in "$@"; do
+	reset_pf $pf
+	set_pf $pf
+	# bind_vfs_vfio $pf
+	setup_vfs $pf
 done
