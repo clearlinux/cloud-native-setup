72659b3ba34aae2d658983a65c0a910643d76160
Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>
2019-02-04 17:12:45 +0000

    Make script executable and qualify run as pod
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>

--- clr-k8s-examples/tests/test-autoscale.sh
+++ clr-k8s-examples/tests/test-autoscale.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 kubectl run php-apache-test --image=k8s.gcr.io/hpa-example --requests=cpu=200m --expose --port=80
 kubectl autoscale deployment php-apache-test --cpu-percent=50 --min=1 --max=10
 kubectl get hpa
 
-#kubectl run -i --tty load-generator --image=busybox /bin/sh
+#kubectl run --generator=run-pod/v1 -i --tty load-generator --image=busybox /bin/sh
 # while true; do wget -q -O- http://php-apache-test.default.svc.cluster.local; done
