025235138ca843b86fc15daddfb7b06b6f2e655e
Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>
2019-02-08 19:55:50 +0000

    Update to latest metrics-server 0.3.x works
    With kubelet service file fixed and correct args set in
    metrics-server-deployment.yaml, autoscale seems to be working reliably.
    
    Fixes: #5
    
    Signed-off-by: Saikrishna Edupuganti <saikrishna.edupuganti@intel.com>

--- clr-k8s-examples/tests/autoscale/test-autoscale.sh
+++ clr-k8s-examples/tests/autoscale/test-autoscale.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-kubectl run php-apache-test --image=k8s.gcr.io/hpa-example --requests=cpu=200m --expose --port=80
-kubectl autoscale deployment php-apache-test --cpu-percent=50 --min=1 --max=10
-kubectl get hpa
-
-#kubectl run --generator=run-pod/v1 -i --tty load-generator --image=busybox /bin/sh
-# while true; do wget -q -O- http://php-apache-test.default.svc.cluster.local; done
+SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
+kubectl apply -f $SCRIPT_DIR
+watch kubectl describe hpa
