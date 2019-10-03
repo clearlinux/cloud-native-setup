33eeccfe3bf667de0134ae72a64114f1b1df4ee8
David Lyle <dklyle0@gmail.com>
2019-10-03 15:43:30 -0600

    Adding support for aysnc metrics via collectd
    This patch adds a new script, scaling/k8s_scaling_rapid.sh, for
    launching pods and collecting metrics. The goal is to two fold.
    The first goal is improve the required runtime duration of scaling
    to large numbers of pods. k8s_scaling.sh can take up to 29 hours
    to scale to 2900 pods. The is largely due to the overhead of
    collecting system utilization stats after each new pod is launched.
    This new script will collect system utilization stats asynchronously.
    The second goal is to make it easier to collect additional system
    utilization stats by leveraging the plugins supported by collectd.
    
    Instead of using the stats daemon set, a new daemon set
    that runs collectd on each node is added. collectd configuration
    is handled by collectd/collectd.conf A configmap is added to the
    K8s cluster containing collectd.conf, so the user of the script
    can test new configurations easily. The configmap is created and
    deleted as part of the test run. The data from collectd is stored on
    each node in the cluster via the csv plugin and the data is
    collected to the master node at the end of the test run.
    
    Several new pages have been added to the metrics_report.pdf
    These pages cover the same metrics as k8s_scale.sh results, but are
    populated with data from collectd. Additionally, network interface
    results are added.
    
    To run the report, in addition to the previous steps of creating
    a new directory and copying the result json file into it, all the
    new <node_name>.tar.gz files must be copied in as well.
    
    Signed-off-by: David Lyle <dklyle0@gmail.com>

---
+++ metrics/collectd/collectd.bash
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Copyright (c) 2019 Intel Corporation
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+THIS_FILE=$(readlink -f ${BASH_SOURCE[0]})
+COLLECTD_DIR=${THIS_FILE%/*}
+
+collectd_pod="collectd"
+
+init_stats() {
+	local wait_time=$1
+
+	# create collectd-config configmap
+	kubectl create configmap collectd-config --from-file=${COLLECTD_DIR}/collectd.conf
+
+	# Launch our stats gathering pod
+	kubectl apply -f ${COLLECTD_DIR}/${collectd_pod}.yaml
+	kubectl rollout status --timeout=${wait_time}s daemonset/${collectd_pod}
+
+	# attempting to provide buffer for collectd to be installed and running,
+    # and CPU collection to build adequate history
+	sleep 12
+}
+
+cleanup_stats() {
+	local delete_wait_time=$1
+
+	# attempting to provide buffer for collectd CPU collection to record adequate history
+	sleep 6
+
+	# get logs before shutting down stats daemonset
+	while read -u 3 name node; do
+		kubectl exec -ti $name -- sh -c "cd /opt/collectd; tar -czvf localhost.tar.gz localhost"
+		kubectl cp $name:/opt/collectd/localhost.tar.gz ${RESULT_DIR}/${node}.tar.gz
+	done 3< <(kubectl get pods --selector name=collectd-pods -o json | jq -r '.items[] | "\(.metadata.name) \(.spec.nodeName)"')
+
+	kubectl delete daemonset --wait=true --timeout=${delete_wait_time}s "${collectd_pod}" || true
+
+	# remove configmap
+	kubectl delete configmap collectd-config
+}
